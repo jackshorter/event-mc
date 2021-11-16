c2826a0fbc64ca66b4faba9fa1b2d7e6262aafbc
jackshorter <55813477+jackshorter@users.noreply.github.com>
2021-11-16 11:45:05 +0000

    initial commit

---
+++ src/main/java/dev/event.java
@@ -0,0 +1,18 @@
+package dev;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class event extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        System.out.println("starting...");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
