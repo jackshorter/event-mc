af20d6a2bce1b6e726f087f75ef1a060087fe281
jackshorter <55813477+jackshorter@users.noreply.github.com>
2021-11-17 20:01:16 +0000

    fallout from class name change

--- src/main/java/dev/rgd/api/teams/Teams.java
+++ src/main/java/dev/rgd/api/teams/Teams.java
@@ -54,72 +54,72 @@ public class Teams {
         String openBracket = ChatColor.WHITE + "[";
         String closeBracket = ChatColor.WHITE + "] ";
 
-        Team spectator = scoreboard.getTeam(TeamEnum.spectator.name());
+        Team spectator = scoreboard.getTeam(EventTeams.spectator.name());
         spectator.setDisplayName("Spectator");
         spectator.setPrefix(openBracket + "Spectator" + closeBracket);
         spectator.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team red = scoreboard.getTeam(TeamEnum.red.name());
+        Team red = scoreboard.getTeam(EventTeams.red.name());
         red.setDisplayName("Red");
         red.setColor(ChatColor.RED);
         red.setPrefix(openBracket + "Spectator" + closeBracket);
         red.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team orange = scoreboard.getTeam(TeamEnum.orange.name());
+        Team orange = scoreboard.getTeam(EventTeams.orange.name());
         orange.setDisplayName("Orange");
         orange.setColor(ChatColor.GOLD);
         orange.setPrefix(openBracket + ChatColor.GOLD + "ORANGE" + closeBracket);
         orange.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team yellow = scoreboard.getTeam(TeamEnum.yellow.name());
+        Team yellow = scoreboard.getTeam(EventTeams.yellow.name());
         yellow.setDisplayName("Yellow");
         yellow.setColor(ChatColor.YELLOW);
         yellow.setPrefix(openBracket + ChatColor.YELLOW + "YELLOW" + closeBracket);
         yellow.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team darkGreen = scoreboard.getTeam(TeamEnum.green.name());
+        Team darkGreen = scoreboard.getTeam(EventTeams.green.name());
         darkGreen.setDisplayName("Green");
         darkGreen.setColor(ChatColor.DARK_GREEN);
         darkGreen.setPrefix(openBracket + ChatColor.DARK_GREEN + "GREEN" + closeBracket);
         darkGreen.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team lime = scoreboard.getTeam(TeamEnum.lime.name());
+        Team lime = scoreboard.getTeam(EventTeams.lime.name());
         lime.setDisplayName("Lime");
         lime.setColor(ChatColor.GREEN);
         lime.setPrefix(openBracket + ChatColor.GREEN + "LIME" + closeBracket);
         lime.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team blue = scoreboard.getTeam(TeamEnum.blue.name());
+        Team blue = scoreboard.getTeam(EventTeams.blue.name());
         blue.setDisplayName("Blue");
         blue.setColor(ChatColor.BLUE);
         blue.setPrefix(openBracket + ChatColor.BLUE + "BLUE" + closeBracket);
         blue.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team aqua = scoreboard.getTeam(TeamEnum.aqua.name());
+        Team aqua = scoreboard.getTeam(EventTeams.aqua.name());
         aqua.setDisplayName("Teal");
         aqua.setColor(ChatColor.AQUA);
         aqua.setPrefix(openBracket + ChatColor.AQUA + "TEAL" + closeBracket);
         aqua.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team purple = scoreboard.getTeam(TeamEnum.purple.name());
+        Team purple = scoreboard.getTeam(EventTeams.purple.name());
         purple.setDisplayName("Purple");
         purple.setColor(ChatColor.DARK_PURPLE);
         purple.setPrefix(openBracket + ChatColor.DARK_PURPLE + "PURPLE" + closeBracket);
         purple.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team pink = scoreboard.getTeam(TeamEnum.pink.name());
+        Team pink = scoreboard.getTeam(EventTeams.pink.name());
         pink.setDisplayName("Pink");
         pink.setColor(ChatColor.LIGHT_PURPLE);
         pink.setPrefix(openBracket + ChatColor.LIGHT_PURPLE + "PINK" + closeBracket);
         pink.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team white = scoreboard.getTeam(TeamEnum.white.name());
+        Team white = scoreboard.getTeam(EventTeams.white.name());
         white.setDisplayName("White");
         white.setColor(ChatColor.WHITE);
         white.setPrefix(openBracket + "WHITE" + closeBracket);
         white.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
 
-        Team gray = scoreboard.getTeam(TeamEnum.gray.name());
+        Team gray = scoreboard.getTeam(EventTeams.gray.name());
         gray.setDisplayName("Grey");
         gray.setColor(ChatColor.GRAY);
         gray.setPrefix(openBracket + ChatColor.GRAY + "GRAY" + closeBracket);
@@ -127,7 +127,7 @@ public class Teams {
 
     }
 
-    public void addPlayer(Player player, TeamEnum team){
+    public void addPlayer(Player player, EventTeams team){
         for (Team teams : scoreboard.getTeams()){
             if (teams.getName() != team.name()) return;
             teams.addPlayer(player);
